101416f78a7d887aceb543bf3f12e9689362fbd0
torga <torgaigor@gmail.com>
2018-04-21 18:24:51 -0300

    adding comments

--- Buffer.java
+++ Buffer.java
@@ -8,7 +8,7 @@ public class Buffer {
     private ArrayBlockingQueue<Integer> buffer = 
             new ArrayBlockingQueue<Integer>(30);
     
-    
+    //método para levar e verificar se é zero
     public int leva(){
         int numero = 0;
         try {
@@ -18,7 +18,7 @@ public class Buffer {
         }
         return numero;
     }
-    
+    //método para colocar na fila e verificar
     public void coloca(int numero){
         try {
             buffer.put(numero);
